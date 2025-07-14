a630ead169167c225b83542102d3344a312100e6
rokemagik <emidqtrabajo@gmail.com>
2025-07-13 21:10:13 -0300

    Finalizando Proyecto

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@
 
 let menu = false;
 
+const historial = []
+const historialmMax = 5
+
 while (!menu) {
     let opciones = prompt(
         '--Calculadora--\n' +
@@ -9,25 +12,30 @@ while (!menu) {
         '1.Suma\n' +
         '2.Resta\n' +
         '3.Multiplicacion\n' +
-        '4.Divicion\n'
+        '4.Divicion\n' +
+        '5.Historial(Ultimas 5 operaciones)\n' 
     )
     
     switch(opciones) {
         case "Suma":
         case "1":
-            Suma()
+            Suma();
             break
         case "Resta":
         case "2":
-            Resta()
+            Resta();
             break
         case "Multuplicacion":
         case "3":
-            Multiplicacion()
+            Multiplicacion();
             break
         case "Divicion":
         case "4":
-            Divicion()
+            Divicion();
+            break
+        case "Historial":
+        case "5":
+            alert(historial.join("\n"));
             break
     }
 }
@@ -47,7 +55,17 @@ function Suma() {
         return
     }
 
-    alert(n1 + n2);
+    let resultado = n1 + n2;
+    alert(resultado);
+
+    if (historial.length >= historialmMax) {
+        historial.shift();
+    } else {
+
+    }
+
+    historial.push("Suma: " + n1 + " + " + n2 + "\n" + "Resultado: " + resultado + "\n")
+
 }
 
 function Resta() {
@@ -65,7 +83,16 @@ function Resta() {
         return
     }
 
-    alert(n1 - n2);
+    let resultado = n1 - n2;
+    alert(resultado);
+
+    if (historial.length >= historialmMax) {
+        historial.shift();
+    } else {
+
+    }
+
+    historial.push("Resta: " + n1 + " - " + n2 + "\n" + "Resultado: " + resultado + "\n")
 }
 
 function Multiplicacion() {
@@ -83,7 +110,16 @@ function Multiplicacion() {
         return
     }
 
-    alert(n1 * n2);
+    let resultado = n1 * n2;
+    alert(resultado);
+
+    if (historial.length >= historialmMax) {
+        historial.shift();
+    } else {
+
+    }
+
+    historial.push("Multipliacion: " + n1 + " * " + n2 + "\n" + "Resultado: " + resultado + "\n")
 }
 
 function Divicion() {
@@ -101,5 +137,14 @@ function Divicion() {
         return
     }
 
-    alert(n1 / n2);
+    let resultado = n1 / n2;
+    alert(resultado);
+
+    if (historial.length >= historialmMax) {
+        historial.shift();
+    } else {
+
+    }
+
+    historial.push("Divicion: " + n1 + " / " + n2 + "\n" + "Resultado: " + resultado + "\n")
 }
